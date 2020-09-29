fb54d73b0df98078b8605bf6424717ca0d59cc1d
yongde2900 <com414141@gmail.com>
2020-09-29 21:07:30 +0800

    Setting up the project and enviroment

---
+++ app.js
@@ -0,0 +1,25 @@
+const { static } = require('express')
+// Require packages used in the project
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Require handlebars
+const exphbs =require('express-handlebars')
+
+// Setting static files
+app.use(express.static('public'))
+
+// Setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// Route setting
+app.get('/' ,(req,res) => {
+    res.render('index')
+})
+
+// Start and listen on express server
+app.listen(port, () => {
+    console.log(`Express is listening on http:localhost:${port}`)
+})
