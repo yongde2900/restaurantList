5eb5c45240d8947f4296f69c536cd69b192be7df
yongde2900 <com414141@gmail.com>
2020-10-04 22:05:22 +0800

    feat : add mongoose

---
+++ models/restaurant.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    name_en:{
+        type: String,
+        required: false
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: false
+    },
+    location:{
+        type: String,
+        required: false
+    },
+    google_map:{
+        type: String,
+        required: false
+    },
+    rating:{
+        type: Number,
+        required: false
+    },
+    description:{
+        type: String,
+        required: false
+    }
+    
+})
