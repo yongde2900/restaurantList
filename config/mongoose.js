d8d7bfbb10df6153dacb2c46294f0abce76f62ed
yongde2900 <com414141@gmail.com>
2020-10-19 22:18:34 +0800

    fix: MONGODB_URI

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/restaurant-list'
 
-mongoose.connect('mongodb://localhost/restaurant-list' || process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error',() => console.log('mongodb error'))
 db.once('open', () => console.log('mongodb connected',))
